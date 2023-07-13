9df12cdd8bf2d8fecf31eaf681a84e7af7276766
Lamin Jawla <laminjawla1001@gmail.com>
2023-07-13 10:58:25 +0000

    Added header files

--- main.h
+++ main.h
@@ -1,4 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*Header Files*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*Function Prototypes*/
+
 #endif
